8d0337cfbdd25629327ed088d9c881b13cde64be
Roney-juma <ochirone10@gmail.com>
2021-04-24 14:40:47 +0300

    added class form to html

--- script.js
+++ script.js
@@ -1,122 +1,124 @@
 //Business logic
-function Pizza(type,toppings,crust,size){
-    this.type=type
-    this.toppings=toppings
-    this.crust=crust
-    this.size=size
-}
-
-Pizza.prototype.Gettotalcost()= function(){
-    return GetToppingscost()+GetSizecost()+GetCrustcost()
-}
-
-Pizza.prototype.GetCrustcost()= function(){
-    if (type=="Cheese"){
-        if(crust==crispy){
-            return 100
-        }
-        if(crust=="stuffed"){
-            return 150
-        }
-        if(crust=="Flatbread"){
-            return 180
-        }
-    }
-    if (type=="Pepperoni"){
-        if(crust=="crispy"){
-            return 120
-        }
-        if(crust=="stuffed"){
-            return 170
-        }
-        if(crust=="Flatbread"){
-            return 200
-        }
-    }
-    if (type=="Veggie"){
-        if(crust=="Crispy"){
-            return 150
-        }
-        if(crust=="Stuffed"){
-            return 180
-        }
-        if(crust=="Flatbread"){
-            return 200
-        }
-    }
-}
-Pizza.prototype.GetToppingscost()= function(){
-    if (type=="Cheese"){
-        if(topping=="Mushroom"){
-            return 30
-        }
-        if(crust=="Brocoli"){
-            return 50
-        }
-        if(crust=="Pancetta"){
-            return 70
-        }
-    }
-    if (type=="Pepperoni"){
-        if(crust=="Mushroom"){
-            return 40
-        }
-        if(crust=="Brocoli"){
-            return 60
-        }
-        if(crust=="Pancetta"){
-            return 80
-        }
-    }
-    if (type=="Veggie"){
-        if(crust=="Mushroom"){
-            return 60
-        }
-        if(crust=="Brocoli"){
-            return 80
-        }
-        if(crust=="Pancetta"){
-            return 100
-        }
-    }
-}
-Pizza.prototype.GetSizecost()= function (){
-    if (type=="Cheese"){
-        if(size=="Small"){
-            return 500
-        }
-        if(size=="Medium"){
-            return 750
-        }
-        if(size=="Large"){
-            return 970
-        }
-    }
-    if (type=="Pepperoni"){
-        if(size=="Small"){
-            return 550
-        }
-        if(size=="Medium"){
-            return 800
-        }
-        if(size=="Large"){
-            return 1000
-        }
-    }
-    if (type=="Veggie"){
-        if(size=="Small"){
-            return 600
-        }
-        if(size=="Medium"){
-            return 850
-        }
-        if(size=="Large"){
-            return 1050
-        }
-    }
-}
-
 $(Document).ready(function (){
+    function Pizza(type,toppings,crust,size){
+        this.type=type
+        this.toppings=toppings
+        this.crust=crust
+        this.size=size
+    }
+
+    Pizza.prototype.Gettotalcost= function(){
+        return this.GetToppingscost + this.GetSizecost + this.GetCrustcost
+    }
+
+    Pizza.prototype.GetCrustcost= function(){
+        if (type=="Cheese"){
+            if(crust==crispy){
+                return 100
+            }
+            if(crust=="stuffed"){
+                return 150
+            }
+            if(crust=="Flatbread"){
+                return 180
+            }
+        }
+        if (type=="Pepperoni"){
+            if(crust=="crispy"){
+                return 120
+            }
+            if(crust=="stuffed"){
+                return 170
+            }
+            if(crust=="Flatbread"){
+                return 200
+            }
+        }
+        if (type=="Veggie"){
+            if(crust=="Crispy"){
+                return 150
+            }
+            if(crust=="Stuffed"){
+                return 180
+            }
+            if(crust=="Flatbread"){
+                return 200
+            }
+        }
+    }
+    Pizza.prototype.GetToppingscost= function(){
+        if (type=="Cheese"){
+            if(topping=="Mushroom"){
+                return 30
+            }
+            if(topping=="Brocoli"){
+                return 50
+            }
+            if(topping=="Pancetta"){
+                return 70
+            }
+        }
+        if (type=="Pepperoni"){
+            if(topping=="Mushroom"){
+                return 40
+            }
+            if(topping=="Brocoli"){
+                return 60
+            }
+            if(topping=="Pancetta"){
+                return 80
+            }
+        }
+        if (type=="Veggie"){
+            if(crust=="Mushroom"){
+                return 60
+            }
+            if(crust=="Brocoli"){
+                return 80
+            }
+            if(crust=="Pancetta"){
+                return 100
+            }
+        }
+    }
+    Pizza.prototype.GetSizecost= function (){
+        if (type=="Cheese"){
+            if(size=="Small"){
+                return 500
+            }
+            if(size=="Medium"){
+                return 750
+            }
+            if(size=="Large"){
+                return 970
+            }
+        }
+        if (type=="Pepperoni"){
+            if(size=="Small"){
+                return 550
+            }
+            if(size=="Medium"){
+                return 800
+            }
+            if(size=="Large"){
+                return 1000
+            }
+        }
+        if (type=="Veggie"){
+            if(size=="Small"){
+                return 600
+            }
+            if(size=="Medium"){
+                return 850
+            }
+            if(size=="Large"){
+                return 1050
+            }
+        }
+    }
+
+    //User Interface logic
+
     $(".pic1").click(function(){
         $(".pic1").hide(100)
         $(".p1").show(100)
