caec811ee7752fa98ab147d7e6ea03b1820adbc4
Roney-juma <ochirone10@gmail.com>
2021-04-26 15:27:28 +0300

    adding html and js for transaction

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Business logic
-$(Document).ready(function (){
     function Pizza(type,toppings,crust,size){
         this.type=type
         this.toppings=toppings
@@ -70,13 +69,13 @@ $(Document).ready(function (){
             }
         }
         if (type=="Veggie"){
-            if(crust=="Mushroom"){
+            if(topping=="Mushroom"){
                 return 60
             }
-            if(crust=="Brocoli"){
+            if(topping=="Brocoli"){
                 return 80
             }
-            if(crust=="Pancetta"){
+            if(topping=="Pancetta"){
                 return 100
             }
         }
@@ -119,32 +118,33 @@ $(Document).ready(function (){
 
     //User Interface logic
 
-    $(".pic1").click(function(){
-        $(".pic1").hide(100)
-        $(".p1").show(100)
-    });
-    $(".p1").click(function(){
-        $(".p1").hide(100)
-        $(".pic1").show(100)
-    });
+    $(document).ready(function (){
+        $(".pic1").click(function(){
+            $(".pic1").hide(100)
+            $(".p1").show(100)
+        });
+        $(".p1").click(function(){
+            $(".p1").hide(100)
+            $(".pic1").show(100)
+        });
+        
+        $(".pic2").click(function(){
+            $(".pic2").hide(100)
+            $(".p2").show(100)
+        });
+        $(".p2").click(function(){
+            $(".p2").hide(100)
+            $(".pic2").show(100)
+        });
+        
     
-    $(".pic2").click(function(){
-        $(".pic2").hide(100)
-        $(".p2").show(100)
-    });
-    $(".p2").click(function(){
-        $(".p2").hide(100)
-        $(".pic2").show(100)
-    });
-    
-
-    $(".pic3").click(function(){
-        $(".pic3").hide(100)
-        $(".p3").show(100)
-    });
-    $(".p3").click(function(){
-        $(".p3").hide(100)
-        $(".pic3").show(100)
-    });
-})
+        $(".pic3").click(function(){
+            $(".pic3").hide(100)
+            $(".p3").show(100)
+        });
+        $(".p3").click(function(){
+            $(".p3").hide(100)
+            $(".pic3").show(100)
+        });
+    })
 
